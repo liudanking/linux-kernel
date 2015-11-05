6b73d72203a9ffd8910834920e587cf20823a46e
liudanking <liudanking@gmail.com>
2015-11-05 13:56:24 +0800

    use ngg_printk instead of printk to provide a quick switch on/off kernel log

--- net/ipv4/tcp_ngg.c
+++ net/ipv4/tcp_ngg.c
@@ -3,6 +3,7 @@
  *
  */
 
+
 #include <linux/mm.h>
 #include <linux/module.h>
 #include <linux/skbuff.h>
@@ -10,6 +11,9 @@
 
 #include <net/tcp.h>
 
+
+
+#define NGG_DBG 1
 #define NGG_MAX_CWND 6553500U
 
 static int alpha = 2;
@@ -32,6 +36,20 @@ struct ngg {
 	u32 srate;			/* smoothed rate according to ack_cnt */
 	u8 loss_cnt;		/* loss packet count */
 };
+
+
+static int ngg_printk(char *fmt, ...)
+{
+#if NGG_DBG
+	va_list args;
+	int n;
+    va_start(args, fmt);
+    n = printk(fmt, args);
+    va_end(args);
+    return n;
+#endif
+    return 0;
+}
 
 /* There are several situations when we must "re-start" ngg:
  *
@@ -93,7 +111,7 @@ static void tcp_ngg_cong_avoid(struct sock *sk, u32 ack, u32 acked)
 
 	cnt_log++;
 	if (cnt_log % 500 == 0) {
-		printk("cong_avoid, cwnd:%u, srate:%uKB\n", tp->snd_cwnd, ngg->srate);
+		ngg_printk("cong_avoid, cwnd:%u, srate:%uKB\n", tp->snd_cwnd, ngg->srate);
 	}
 
 
@@ -105,7 +123,7 @@ static u32 tcp_ngg_ssthresh(struct sock *sk)
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct ngg *ngg = inet_csk_ca(sk);
 	if (ngg->loss_cnt > gamma) {
-		printk("cwnd MD\n");
+		ngg_printk("cwnd MD\n");
 		ngg->loss_cnt = 0;
 		tp->snd_cwnd = max(tp->snd_cwnd / 2, 8192U);
 	} else {
@@ -113,7 +131,7 @@ static u32 tcp_ngg_ssthresh(struct sock *sk)
 		tp->snd_cwnd = max(tp->snd_cwnd, 8192U);
 	}
 
-	printk("ssthresh:%u, loss_cnt:%u\n", tp->snd_cwnd, ngg->loss_cnt);
+	ngg_printk("ssthresh:%u, loss_cnt:%u\n", tp->snd_cwnd, ngg->loss_cnt);
 	return tp->snd_cwnd;
 }
 
@@ -122,19 +140,19 @@ static void tcp_ngg_set_state(struct sock *sk, u8 new_state)
 	struct tcp_sock *tp = tcp_sk(sk);
 	switch (new_state) {
 		case TCP_CA_Open:
-			printk("TCP_CA_Open, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("TCP_CA_Open, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case TCPF_CA_Disorder:
-			printk("TCPF_CA_Disorder, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("TCPF_CA_Disorder, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case TCPF_CA_CWR:
-			printk("TCPF_CA_CWR, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("TCPF_CA_CWR, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case TCPF_CA_Recovery:
-			printk("TCPF_CA_Recovery, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("TCPF_CA_Recovery, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case TCPF_CA_Loss:
-			printk("TCPF_CA_Loss, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("TCPF_CA_Loss, cwnd:%u\n", tp->snd_cwnd);
 			break;
 	}
 }
@@ -145,29 +163,29 @@ static void tcp_ngg_cwnd_event(struct sock *sk, enum tcp_ca_event ev)
 	struct ngg *ngg = inet_csk_ca(sk);
 	switch (ev) {
 		case CA_EVENT_TX_START:
-			printk("CA_EVENT_TX_START, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("CA_EVENT_TX_START, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case CA_EVENT_CWND_RESTART:
-			printk("CA_EVENT_CWND_RESTART, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("CA_EVENT_CWND_RESTART, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case CA_EVENT_COMPLETE_CWR:
-			printk("CA_EVENT_COMPLETE_CWR, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("CA_EVENT_COMPLETE_CWR, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case CA_EVENT_LOSS:
 			ngg->loss_cnt++;
-			printk("CA_EVENT_LOSS, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("CA_EVENT_LOSS, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case CA_EVENT_ECN_NO_CE:
-			printk("CA_EVENT_ECN_NO_CE, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("CA_EVENT_ECN_NO_CE, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case CA_EVENT_ECN_IS_CE:
-			printk("CA_EVENT_ECN_IS_CE, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("CA_EVENT_ECN_IS_CE, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case CA_EVENT_DELAYED_ACK:
-			printk("CA_EVENT_DELAYED_ACK, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("CA_EVENT_DELAYED_ACK, cwnd:%u\n", tp->snd_cwnd);
 			break;
 		case CA_EVENT_NON_DELAYED_ACK:
-			printk("CA_EVENT_NON_DELAYED_ACK, cwnd:%u\n", tp->snd_cwnd);
+			ngg_printk("CA_EVENT_NON_DELAYED_ACK, cwnd:%u\n", tp->snd_cwnd);
 			break;
 	}
 }
@@ -185,7 +203,7 @@ static void tcp_ngg_pkts_acked(struct sock *sk, u32 num_acked, s32 rtt_us)
 
 	cnt_log++;
 	if (cnt_log % 500  == 0) {
-		printk("pkts_acked, num_acked:%u, rtt_us:%d, rate:%uKB\n", num_acked, rtt_us, r/1024);
+		ngg_printk("pkts_acked, num_acked:%u, rtt_us:%d, rate:%uKB\n", num_acked, rtt_us, r/1024);
 	}
 }
 
